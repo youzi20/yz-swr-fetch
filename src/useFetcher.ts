b20b08f530fcb8921af19ff3b6d2705ba58e418b
youzi <2361846314@qq.com>
2025-05-29 17:59:06 +0800

    更新版本号至1.1.14，优化useAuthStore和useFetcher逻辑，调整导入方式

--- src/useFetcher.ts
+++ src/useFetcher.ts
@@ -23,13 +23,19 @@ export function useFetcher<T>(type: FetcherType, options?: OptionsType) {
     async (url: string, _options?: OptionsType) => {
       var { headers, handleErrorMessage, ...other } = _options ?? options ?? {};
 
-      if (type === FetcherType.PUBLIC_OR_AUTH && authorization) {
+      if (type === FetcherType.AUTH_CHECK) {
+        if (!authorization) return null;
+
         headers = { authorization, ...headers };
       }
 
-      if (type === FetcherType.AUTH_CHECK || isLogin) {
-        if (!authorization) return null;
+      if (type === FetcherType.AUTH) {
+        if (!isLogin || !authorization) return null;
 
+        headers = { authorization, ...headers };
+      }
+
+      if (type === FetcherType.PUBLIC_OR_AUTH && authorization) {
         headers = { authorization, ...headers };
       }
 
